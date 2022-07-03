f3d12a5fc234d0a341be9a18f10b656676dfbef5
llkay Uzunali <ilkayburakuzunali@gmail.com>
2022-07-03 22:01:16 +0300

    IServiceGeneric > IGenericService

--- AuthServer.Core/Services/IGenericService.cs
+++ AuthServer.Core/Services/IGenericService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Core.Services
 {
-    public interface IServiceGeneric<TEntity,TDto> where TEntity : class where TDto : class
+    public interface IGenericService<TEntity,TDto> where TEntity : class where TDto : class
     {
         Task<Response<TDto>> GetByIdAsync(int id);
         Task<Response<IQueryable<TDto>>> GetAllAsync();
