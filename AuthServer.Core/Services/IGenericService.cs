7626dcd3652c328fcddb8ef13a2f67715b6e07a0
llkay Uzunali <ilkayburakuzunali@gmail.com>
2022-07-23 23:38:37 +0300

    IGenericService TDto fix

--- AuthServer.Core/Services/IGenericService.cs
+++ AuthServer.Core/Services/IGenericService.cs
@@ -13,9 +13,9 @@ namespace AuthServer.Core.Services
         Task<Response<TDto>> GetByIdAsync(int id);
         Task<Response<IQueryable<TDto>>> GetAllAsync();
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
-        Task<Response<TDto>> AddAsync(TEntity entity);
-        Task<Response<NoDataDto>> Remove(TEntity entity);
-        Task<Response<NoDataDto>> Update(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto entity);
+        Task<Response<NoDataDto>> Remove(TDto entity);
+        Task<Response<NoDataDto>> Update(TDto entity);
 
     }
 }
